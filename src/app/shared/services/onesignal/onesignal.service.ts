e8fc280a224232bdc9b4b40e0a10e1ea7d091080
Diegocaceres97 <diegocaceres1997garcia@gmail.com>
2024-06-02 19:23:01 -0500

    chore: little changes for proof

--- src/app/shared/services/onesignal/onesignal.service.ts
+++ src/app/shared/services/onesignal/onesignal.service.ts
@@ -120,9 +120,9 @@ sendNotification(msg: string, title: string, data: any = null, external_id?: any
     //android_channel_id: environment.onesignal.android_channel_id,
     // small_icon: 'mipmap/ic_launcher_round',
     // large_icon: 'mipmap/ic_launcher_round',
-    small_icon: 'mipmap/ic_notification',
+   /*  small_icon: 'mipmap/ic_notification',
     large_icon: 'mipmap/ic_notification_large',
-    ios_sound: 'sound.wav',
+    ios_sound: 'sound.wav', */
     // filters: [
     //   {
     //     field: 'tag',
